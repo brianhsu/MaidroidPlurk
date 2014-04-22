aefc548cc4ac451b037d242208bc02e46ca92efc
BrianHsu <brianhsu.hsu@gmail.com>
2014-04-22 16:57:43 +0800

    Separate ImageCache to in PlurkImageGetter to another file.

--- src/main/scala/util/PlurkImageGetter.scala
+++ src/main/scala/util/PlurkImageGetter.scala
@@ -1,6 +1,7 @@
 package idv.brianhsu.maidroid.plurk.util
 
 import idv.brianhsu.maidroid.plurk._
+import idv.brianhsu.maidroid.plurk.cache._
 import idv.brianhsu.maidroid.ui.util.AsyncUI._
 
 import scala.concurrent._
@@ -21,7 +22,6 @@ class PlurkImageGetter(activity: Activity, adapter: BaseAdapter) extends Html.Im
   private implicit val implicitActivity = activity
   private var mPlaceHolder: Option[Bitmap] = Some(BitmapFactory.decodeResource(activity.getResources, R.drawable.placeholder))
 
-  private val imageCache = new LRUCache[String, Bitmap](5)
   private def placeHolder = {
     mPlaceHolder.filterNot(_.isRecycled) match {
       case Some(bitmap) => bitmap
@@ -56,72 +56,10 @@ class PlurkImageGetter(activity: Activity, adapter: BaseAdapter) extends Html.Im
     }
   }
 
-  private def openStream(imageURL: String) = {
-    if (imageURL.contains("images.plurk.com/tx_")) {
-      val newURL = imageURL.replace("/tx_", "/").replace(".gif", ".jpg")
-      val connection = new URL(newURL).openConnection()
-      connection.getInputStream()
-    } else {
-      val connection = new URL(imageURL).openConnection()
-      connection.getInputStream()
-    }
-  }
-
-  private def calculateOriginSize(url: String): (Int, Int) = {
-    val imgStream = openStream(url)
-    val options = new BitmapFactory.Options
-    options.inJustDecodeBounds = true
-    BitmapFactory.decodeStream(imgStream, null, options)
-    imgStream.close()
-    (options.outWidth, options.outHeight)
-  }
-
-  private def downloadImageFromNetwork(url: String): Bitmap = {
-    val (originWidth, originHeight) = calculateOriginSize(url)
-    val inSampleSize = calculateInSampleSize(originWidth, originHeight, 150, 150)
-    val imgStream = openStream(url)
-    val options = new BitmapFactory.Options
-    options.inSampleSize = inSampleSize
-
-    if (originWidth <= 48 && originHeight <= 48) {
-      options.inDensity = 160
-      options.inScaled = false
-      options.inTargetDensity = 160
-    }
-
-    val imgBitmap = BitmapFactory.decodeStream(imgStream, null, options)
-    imgStream.close()
-    imageCache += (url -> imgBitmap)
-    imgBitmap
-  }
-
-  private def calculateInSampleSize(originWidth: Int, originHeight: Int, 
-                                    requiredWidth: Int, requiredHeight: Int): Int = {
-    var inSampleSize = 1
-
-    if (originHeight > requiredHeight || originWidth > requiredWidth) {
-
-        val halfHeight = originHeight / 2
-        val halfWidth = originWidth / 2
-
-        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-        // height and width larger than the requested height and width.
-        while ((halfHeight / inSampleSize) > requiredHeight && 
-               (halfWidth / inSampleSize) > requiredWidth) {
-            inSampleSize *= 2
-        }
-    }
-
-    inSampleSize
-  }
-
   private def getDrawableFromNetwork(source: String) = {
     val urlDrawable = new URLDrawable(activity.getResources, placeHolder)
-    val bitmapFuture = future { 
-      downloadImageFromNetwork(source) 
-    }
 
-    bitmapFuture.onSuccessInUI { bitmap =>
+    ImageCache.getBitmapFromNetwork(source).onSuccessInUI { bitmap =>
       urlDrawable.updateDrawable(bitmap)
       adapter.notifyDataSetChanged()
     }
@@ -136,11 +74,11 @@ class PlurkImageGetter(activity: Activity, adapter: BaseAdapter) extends Html.Im
   }
 
   override def getDrawable(source: String): Drawable = {
-    val drawableFromMemory = imageCache.get(source).map(getDrawableFromBitmap)
-    drawableFromMemory getOrElse (getDrawableFromNetwork(source))
+    ImageCache.getBitmapFromCache(source) match {
+      case Some(bitmap) => getDrawableFromBitmap(bitmap)
+      case None => getDrawableFromNetwork(source)
+    }
   }
-
-  def getLRUCache = imageCache
 
 }
 
