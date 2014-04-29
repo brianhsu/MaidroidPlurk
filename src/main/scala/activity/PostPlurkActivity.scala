fdb29ca451d351b1f627b453da21d422af91d398
BrianHsu <brianhsu.hsu@gmail.com>
2014-04-29 17:02:41 +0800

    Add post plurk action bar dummy action buttons.

--- src/main/scala/activity/PostPlurkActivity.scala
+++ src/main/scala/activity/PostPlurkActivity.scala
@@ -1,6 +1,7 @@
 package idv.brianhsu.maidroid.plurk.activity
 
 import idv.brianhsu.maidroid.plurk._
+import idv.brianhsu.maidroid.plurk.TypedResource._
 
 import android.app.Activity
 import android.os.Bundle
@@ -13,12 +14,23 @@ import android.view.ViewGroup
 import android.support.v7.app.ActionBar.TabListener
 import android.support.v7.app.ActionBar.Tab
 import android.support.v4.app.FragmentTransaction
+import android.widget.ArrayAdapter
+import android.view.Menu
 
 class PostPublicFragment extends Fragment {
 
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, 
                             savedInstanceState: Bundle): View = {
-    inflater.inflate(R.layout.fragment_post_public, container, false)
+    val view = inflater.inflate(R.layout.fragment_post_public, container, false)
+    val qualifierAdapter = new ArrayAdapter(getActivity, android.R.layout.simple_spinner_item, Array("說", "覺得", "喜歡", "分享"))
+    val respondAdapter = new ArrayAdapter(getActivity, android.R.layout.simple_spinner_item, Array("開放回應", "只有朋友可以回應", "關閉回應功能"))
+
+    val qualifierSpinner = view.findView(TR.fragmentPostPublicQualifier)
+    val respondSpinner = view.findView(TR.fragmentPostPublicRespond)
+
+    qualifierSpinner.setAdapter(qualifierAdapter)
+    respondSpinner.setAdapter(respondAdapter)
+    view
   }
 }
 
@@ -81,6 +93,12 @@ class PostPlurkActivity extends ActionBarActivity
     val adapter = new PostPlurkAdapter(getSupportFragmentManager)
     viewPager.setAdapter(adapter)
     viewPager.setOnPageChangeListener(this)
+  }
+
+  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+    val inflater = getMenuInflater
+    inflater.inflate(R.menu.post_plurk, menu)
+    super.onCreateOptionsMenu(menu)
   }
 
   override def onTabReselected(tab: Tab, ft: FragmentTransaction) {  }
