b5b5e455cc29e66b3a51b7120e38b7dd9c8c30d7
BrianHsu <brianhsu.hsu@gmail.com>
2015-06-09 16:48:58 +0000

    Implement basic my profile page.

--- src/main/scala/activity/CurrentUserActivity.scala
+++ src/main/scala/activity/CurrentUserActivity.scala
@@ -31,16 +31,53 @@ object CurrentUserActivity {
 }
 
 class CurrentUserActivity extends ActionBarActivity 
-                           with TypedViewHolder 
+                          with ConfirmDialog.Listener
+                          with TypedViewHolder 
 {
-  /*
-  private lazy val viewPager = findView(TR.activityUserTimelineViewPager)
-  private lazy val pagerIndicator = findView(TR.activityUserTimelinePagerIndicator)
-  private lazy val pageAdapter = new UserTimelineAdapter(this, getSupportFragmentManager, userID)
-  */
+  private lazy val viewPager = findView(TR.activityCurrentUserViewPager)
+  private lazy val pagerIndicator = findView(TR.activityCurrentUserPagerIndicator)
+  private lazy val pageAdapter = new CurrentUserAdapter(this, getSupportFragmentManager)
+  private lazy val dialogFrame = ToggleView.setupAngryBehavior(this, findView(TR.activityCurrentUserDialogFrame))
+
 
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_current_user)
+    viewPager.setAdapter(pageAdapter)
+    pagerIndicator.setViewPager(viewPager)
+    dialogFrame.setMessages(
+      Message(MaidMaro.Half.Smile, getString(R.string.activityCurrentUserWelcome01)) ::
+      Nil
+    )
+
   }
+
+  override def onResume() {
+    super.onResume()
+    ToggleView.syncDialogVisibility(this, dialogFrame)
+  }
+
+  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+    val inflater = getMenuInflater
+    inflater.inflate(R.menu.activity_current_user, menu)
+    super.onCreateOptionsMenu(menu)
+  }
+
+  override def onOptionsItemSelected(menuItem: MenuItem): Boolean = menuItem.getItemId match {
+    case R.id.activityCurrentUserLogout => Logout.logout(this); false
+    case R.id.activityCurrentUserAbout => AboutActivity.startActivity(this); false
+    case R.id.activityCurrentUserToggleMaid => ToggleView(this, dialogFrame); false
+    case _ => super.onOptionsItemSelected(menuItem)
+  }
+
+  override def onDialogOKClicked(dialogName: Symbol, dialog: DialogInterface, data: Bundle) {
+    dialogName match {
+      case 'LogoutConfirm => 
+        dialog.dismiss()
+        this.finish()
+        Logout.doLogout(this)
+    }
+  }
+
+
 }
--- src/main/scala/fragment/CurrentUserProfileFragment.scala
+++ src/main/scala/fragment/CurrentUserProfileFragment.scala
@@ -16,6 +16,7 @@ import android.view.ViewGroup
 import android.view.Menu
 import android.view.MenuItem
 import android.view.MenuInflater
+import android.view.Gravity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -28,22 +29,19 @@ import idv.brianhsu.maidroid.plurk.TypedResource._
 import idv.brianhsu.maidroid.plurk.util._
 import idv.brianhsu.maidroid.ui.util.AsyncUI._
 import idv.brianhsu.maidroid.ui.util.CallbackConversions._
-import org.bone.soplurk.api.PlurkAPI.PublicProfile
+import org.bone.soplurk.api.PlurkAPI.OwnProfile
 import org.bone.soplurk.constant.TimelinePrivacy
 import org.bone.soplurk.model.User
+import java.util.Date
+import java.text.SimpleDateFormat
 import scala.concurrent._
+import android.app.ProgressDialog
 
 object CurrentUserProfileFragment {
 
   val SendPrivatePlurk = 1
 
-  def newInstance(userID: Long) = {
-    val args = new Bundle
-    val fragment = new CurrentUserProfileFragment
-    args.putLong(UserTimelineActivity.ExtraUserID, userID)
-    fragment.setArguments(args)
-    fragment   
-  }
+  def newInstance() = new CurrentUserProfileFragment
 
   trait Listener {
     def onPostPrivateMessageOK(): Unit
@@ -58,40 +56,276 @@ class CurrentUserProfileFragment extends Fragment {
   private implicit def activity = getActivity.asInstanceOf[ActionBarActivity with CurrentUserProfileFragment.Listener]
 
   private def plurkAPI = PlurkAPIHelper.getPlurkAPI(activity)
-  private def loadingIndicatorHolder = Option(getView).map(_.findView(TR.fragmentUserProfileLoadingIndicator))
-  private def errorNoticeHolder = Option(getView).map(_.findView(TR.fragmentUserProfileErrorNotice))
-  private def displayNameHolder = Option(getView).map(_.findView(TR.userProfileDisplayName))
-  private def nickNameHolder = Option(getView).map(_.findView(TR.userProfileNickName))
-  private def avatarHolder = Option(getView).map(_.findView(TR.userProfileAvatar))
-  private def karmaHolder = Option(getView).map(_.findView(TR.userProfileKarma))
-  private def postsHolder = Option(getView).map(_.findView(TR.userProfilePosts))
-  private def friendsHolder = Option(getView).map(_.findView(TR.userProfileFriends))
-  private def fansHolder = Option(getView).map(_.findView(TR.userProfileFans))
+  private def loadingIndicatorHolder = Option(getView).map(_.findView(TR.fragmentCurrentUserProfileLoadingIndicator))
+  private def errorNoticeHolder = Option(getView).map(_.findView(TR.fragmentCurrentUserProfileErrorNotice))
+  private def displayNameHolder = Option(getView).map(_.findView(TR.currentUserProfileDisplayName))
+  private def nickNameHolder = Option(getView).map(_.findView(TR.currentUserProfileNickName))
+  private def avatarHolder = Option(getView).map(_.findView(TR.currentUserProfileAvatar))
+  private def karmaHolder = Option(getView).map(_.findView(TR.currentUserProfileKarma))
+  private def postsHolder = Option(getView).map(_.findView(TR.currentUserProfilePosts))
+  private def friendsHolder = Option(getView).map(_.findView(TR.currentUserProfileFriends))
+  private def fansHolder = Option(getView).map(_.findView(TR.currentUserProfileFans))
 
-  private def plurkCountsHolder = Option(getView).map(_.findView(TR.userProfilePosts))
-  private def aboutHolder = Option(getView).map(_.findView(TR.fragmentUserProfileAbout))
-  private def privateMessageToSelfButtonHolder = Option(getView).map(_.findView(TR.fragmentUserProfilePrivateMessageToSelf))
+  private def plurkCountsHolder = Option(getView).map(_.findView(TR.currentUserProfilePosts))
+  private def aboutHolder = Option(getView).map(_.findView(TR.fragmentCurrentUserProfileAbout))
+  private def privateMessageToSelfButtonHolder = Option(getView).map(_.findView(TR.fragmentCurrentUserProfilePrivateMessageToSelf))
+  private def fullNameViewHolder = Option(getView).map(_.findView(TR.currentUserProfileFullName))
+  private def birthdayViewHolder = Option(getView).map(_.findView(TR.currentUserProfileBirthday))
+  private def privacyViewHolder = Option(getView).map(_.findView(TR.currentUserProfilePrivacy))
 
-  private lazy val userIDHolder = for {
-    argument <- Option(getArguments)
-    userID   <- Option(argument.getLong(UserTimelineActivity.ExtraUserID))
-  } yield userID
+  private var userBirthdayHolder: Option[Date] = None
+  private var userPrivacy: Option[TimelinePrivacy] = None
+  private var editButtonHolder: Option[MenuItem] = None
 
+  private val dateFormatter = new SimpleDateFormat("yyyy-MM-dd")
 
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setHasOptionsMenu(true)
   }
 
+  override def onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+    inflater.inflate(R.menu.fragment_current_user_profile, menu)
+    editButtonHolder = Option(menu.findItem(R.id.fragmentCurrentUserProfileEdit))
+  }
+
+  override def onOptionsItemSelected(item: MenuItem) = item.getItemId match {
+    case R.id.fragmentCurrentUserProfileEditFullName => startEditFullName(); false
+    case R.id.fragmentCurrentUserProfileEditDisplayName => startEditDisplayName(); false
+    case R.id.fragmentCurrentUserProfileEditBirthday => startEditBirthday(); false
+    case R.id.fragmentCurrentUserProfileEditAbout => startEditAbout(); false
+    case R.id.fragmentCurrentUserProfileEditTimelinePrivacy => startEditPrivacy(); false
+    case _ => super.onOptionsItemSelected(item)
+  }
+
+  private def startEditPrivacy() {
+    import android.app.AlertDialog
+    import android.content.DialogInterface
+    import android.widget.EditText
+    import android.text.TextWatcher
+    import android.text.Editable
+    import android.widget.Spinner
+    import android.widget.ArrayAdapter
+
+    val alertDialogBuilder = new AlertDialog.Builder(activity)
+    val spinner = new Spinner(activity)
+    val items = Array(
+      activity.getString(R.string.timelinePrivacyWorld), 
+      activity.getString(R.string.timelinePrivacyOnlyFriends)
+    )
+    val adapter = new ArrayAdapter(activity, android.R.layout.simple_list_item_1, items)
+
+    spinner.setAdapter(adapter)
+
+    userPrivacy.foreach { privacy =>
+      val defaultSelection = privacy match {
+        case TimelinePrivacy.World => 0
+        case TimelinePrivacy.OnlyFriends => 1
+      }
+      spinner.setSelection(defaultSelection)
+    }
+
+    alertDialogBuilder.setTitle(R.string.profileEditPrivacy)
+                      .setView(spinner)
+
+    alertDialogBuilder.setPositiveButton(R.string.ok, null)
+    alertDialogBuilder.setNegativeButton(R.string.cancel, null)
+
+    val alertDialog = alertDialogBuilder.create
+
+    def updateTimelinePrivacy(newPrivacy: TimelinePrivacy) {
+      val progress = ProgressDialog.show(
+        activity, 
+        activity.getString(R.string.pleaseWait), 
+        activity.getString(R.string.profileSaving),
+        true, false
+      )
+      val future = Future { plurkAPI.Users.update(privacy = Some(newPrivacy)) }
+
+      future.onSuccessInUI { e: Any => 
+        progress.dismiss() 
+        alertDialog.dismiss()
+        updateProfile()
+      }
+
+      future.onFailureInUI { case e: Exception => 
+        progress.dismiss() 
+        Toast.makeText(activity, R.string.profileSavingFailed, Toast.LENGTH_LONG).show()
+      }
+    }
+
+    alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+      override def onShow(dialog: DialogInterface) {
+        val okButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE)
+        okButton.setOnClickListener { view: View =>
+          val newPrivacy = spinner.getSelectedItemPosition match {
+            case 0 => TimelinePrivacy.World
+            case 1 => TimelinePrivacy.OnlyFriends
+          }
+
+          if (newPrivacy == TimelinePrivacy.OnlyFriends) {
+            val warningDialog = new AlertDialog.Builder(activity)
+            warningDialog.setTitle(R.string.profilePrivateTimelineConfirmTitle)
+                         .setMessage(R.string.profilePrivateTimelineConfirmContent)
+            warningDialog.setNegativeButton(R.string.ok, null)
+            warningDialog.setPositiveButton(R.string.cancel, new DialogInterface.OnClickListener() {
+              override def onClick(dialog: DialogInterface, which: Int) {
+                updateTimelinePrivacy(newPrivacy)
+              }
+            })
+            warningDialog.show()
+          } else {
+            updateTimelinePrivacy(newPrivacy)
+          }
+        }
+      }
+    })
+
+    alertDialog.show()
+
+  }
+
+  private def startEditBirthday() {
+    import android.app.DatePickerDialog
+    import android.widget.DatePicker
+    import java.util.Calendar
+
+    val calendar = Calendar.getInstance()
+    userBirthdayHolder.foreach(date => calendar.setTime(date))
+    val mYear = calendar.get(Calendar.YEAR)
+    val mMonth = calendar.get(Calendar.MONTH)
+    val mDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+    val onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+      override def onDateSet(view: DatePicker, year: Int, month: Int, day: Int): Unit = {
+        calendar.set(Calendar.YEAR, year)
+        calendar.set(Calendar.MONTH, month)
+        calendar.set(Calendar.DAY_OF_MONTH, day)
+        val progress = ProgressDialog.show(
+          activity, 
+          activity.getString(R.string.pleaseWait), 
+          activity.getString(R.string.profileSaving), 
+          true, false
+        )
+        val future = Future { plurkAPI.Users.update(birthday = Some(calendar.getTime)) }
+
+        future.onSuccessInUI { e: Any => 
+          progress.dismiss() 
+          updateProfile()
+        }
+
+        future.onFailureInUI { case e: Exception => 
+          progress.dismiss() 
+          Toast.makeText(activity, R.string.profileSavingFailed, Toast.LENGTH_LONG).show()
+        }
+
+      }
+    }
+
+    val datePickerDialog = new DatePickerDialog(activity, onDateSetListener, mYear,mMonth, mDay);
+    datePickerDialog.setTitle(R.string.profileEditBirthday)
+    datePickerDialog.show()
+  }
+
+  private def startEditDialog(title: String, defaultValue: String, lines: Int, callback: String => Any) {
+    import android.app.AlertDialog
+    import android.content.DialogInterface
+    import android.widget.EditText
+    import android.text.TextWatcher
+    import android.text.Editable
+
+    val alertDialogBuilder = new AlertDialog.Builder(activity)
+    val editText = new EditText(activity)
+    editText.setText(defaultValue)
+    if (lines == 1) {
+      editText.setSingleLine()
+    } else {
+      editText.setLines(lines)
+      editText.setGravity(Gravity.TOP)
+    }
+
+    alertDialogBuilder.setTitle(title)
+                      .setView(editText)
+
+    alertDialogBuilder.setPositiveButton(R.string.ok, null)
+    alertDialogBuilder.setNegativeButton(R.string.cancel, null)
+
+    val alertDialog = alertDialogBuilder.create
+    alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+      override def onShow(dialog: DialogInterface) {
+        val okButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE)
+        okButton.setOnClickListener { view: View =>
+          val progress = ProgressDialog.show(
+            activity, 
+            activity.getString(R.string.pleaseWait), 
+            activity.getString(R.string.profileSaving), 
+            true, false
+          )
+          val future = Future { callback(editText.getText.toString) }
+
+          future.onSuccessInUI { e: Any => 
+            progress.dismiss() 
+            alertDialog.dismiss()
+            updateProfile()
+          }
+
+          future.onFailureInUI { case e: Exception => 
+            progress.dismiss() 
+            Toast.makeText(activity, R.string.profileSavingFailed, Toast.LENGTH_LONG).show()
+          }
+        }
+      }
+    })
+
+    editText.addTextChangedListener(new TextWatcher{
+      override def afterTextChanged(s: Editable) {
+        val shouldEnableOKButton = s.toString.trim.size > 0
+        alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(shouldEnableOKButton)
+      }
+      override def beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+      override def onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    })
+
+    alertDialog.show()
+  }
+
+  private def startEditAbout() {
+    val defaultValue = aboutHolder.map(_.getText.toString).getOrElse("")
+    startEditDialog(
+      activity.getString(R.string.profileEditAbout), 
+      defaultValue, 5, 
+      newValue => plurkAPI.Users.update(about = Some(newValue)).get
+    )
+  }
+
+  private def startEditFullName() {
+    val defaultValue = fullNameViewHolder.map(_.getText.toString).getOrElse("")
+    startEditDialog(
+      activity.getString(R.string.profileEditFullName), 
+      defaultValue, 1, 
+      newValue => plurkAPI.Users.update(fullName = Some(newValue)).get
+    )
+  }
+
+  private def startEditDisplayName() {
+    val defaultValue = displayNameHolder.map(_.getText.toString).getOrElse("")
+    startEditDialog(
+      activity.getString(R.string.profileEditDisplayName), 
+      defaultValue, 1, 
+      newValue => plurkAPI.Users.update(displayName = Some(newValue)).get
+    )
+  }
+
   private def showErrorNotice(message: String) {
     loadingIndicatorHolder.foreach(_.hide())
+    editButtonHolder.foreach(_.setVisible(false))
     errorNoticeHolder.foreach(_.setVisibility(View.VISIBLE))
     errorNoticeHolder.foreach { errorNotice =>
       errorNotice.setMessageWithRetry(message) { retryButton =>
         retryButton.setEnabled(false)
         errorNoticeHolder.foreach(_.setVisibility(View.GONE))
         loadingIndicatorHolder.foreach(_.show())
-        updateProfile(userIDHolder.getOrElse(-1L))
+        updateProfile()
       }
     }
   }
@@ -107,7 +341,7 @@ class CurrentUserProfileFragment extends Fragment {
     }
   }
 
-  private def setupBasicInfo(profile: PublicProfile) = {
+  private def setupBasicInfo(profile: OwnProfile) = {
     val basicInfo = profile.userInfo.basicInfo
     val nickname = basicInfo.nickname
     val displayName = basicInfo.displayName.getOrElse(nickname)
@@ -117,6 +351,23 @@ class CurrentUserProfileFragment extends Fragment {
     karmaHolder.foreach{ _.setText(f"${basicInfo.karma}%.2f") }
     friendsHolder.foreach{ _.setText(profile.friendsCount.toString) }
     fansHolder.foreach{ _.setText(profile.fansCount.toString) }
+    fullNameViewHolder.foreach(_.setText(profile.userInfo.basicInfo.fullName))
+    userBirthdayHolder = profile.userInfo.basicInfo.birthday
+    userPrivacy = Some(profile.privacy)
+
+    val privacyText = profile.privacy match {
+      case TimelinePrivacy.World => activity.getString(R.string.timelinePrivacyWorld)
+      case TimelinePrivacy.OnlyFriends => activity.getString(R.string.timelinePrivacyOnlyFriends)
+    }
+
+    privacyViewHolder.foreach(_.setText(privacyText))
+
+    for {
+      birthday <- userBirthdayHolder
+      birthdayView <- birthdayViewHolder
+    } {
+      birthdayView.setText(dateFormatter.format(birthday))
+    }
 
     for {
       plurksCount <- profile.userInfo.plurksCount
@@ -135,30 +386,26 @@ class CurrentUserProfileFragment extends Fragment {
 
   }
 
-  private def setupPrivateMessageButton(button: Button, profile: PublicProfile) {
+  private def setupPrivateMessageButton(button: Button, profile: OwnProfile) {
     button.setOnClickListener { view: View =>
 
-      if (profile.areFriends.getOrElse(false) || profile.userInfo.basicInfo.id == PlurkAPIHelper.plurkUserID) {
-        val intent = new Intent(activity, classOf[PostPlurkActivity])
-        val basicInfo = profile.userInfo.basicInfo
-        val displayName = basicInfo.displayName getOrElse basicInfo.nickname
+      val intent = new Intent(activity, classOf[PostPlurkActivity])
+      val basicInfo = profile.userInfo.basicInfo
+      val displayName = basicInfo.displayName getOrElse basicInfo.nickname
 
-        intent.putExtra(PostPlurkActivity.PrivatePlurkUserID, profile.userInfo.basicInfo.id)
-        intent.putExtra(PostPlurkActivity.PrivatePlurkFullName, profile.userInfo.basicInfo.fullName)
-        intent.putExtra(PostPlurkActivity.PrivatePlurkDisplayName, displayName)
+      intent.putExtra(PostPlurkActivity.PrivatePlurkUserID, profile.userInfo.basicInfo.id)
+      intent.putExtra(PostPlurkActivity.PrivatePlurkFullName, profile.userInfo.basicInfo.fullName)
+      intent.putExtra(PostPlurkActivity.PrivatePlurkDisplayName, displayName)
 
-        startActivityForResult(intent, CurrentUserProfileFragment.SendPrivatePlurk)
-      } else {
-        Toast.makeText(activity, R.string.activityUserTimelineSendPMToNotFriend, Toast.LENGTH_LONG).show()
-        activity.onPostPrivateMessageToNotFriend()
-      }
+      startActivityForResult(intent, CurrentUserProfileFragment.SendPrivatePlurk)
     }
   }
 
-  private def updateProfile(userID: Long) {
+  private def updateProfile() {
 
-    val userProfile = Future { plurkAPI.Profile.getPublicProfile(userID).get }
+    val userProfile = Future { plurkAPI.Profile.getOwnProfile.get }
 
+    editButtonHolder.foreach(_.setVisible(false))
     userProfile.onSuccessInUI { profile =>
 
       val basicInfo = profile.userInfo.basicInfo
@@ -170,7 +417,7 @@ class CurrentUserProfileFragment extends Fragment {
         case Some(avatarBitmap) => setAvatarFromCache(avatarBitmap)
         case None => setAvatarFromNetwork(activity, basicInfo)
       }
-
+      editButtonHolder.foreach(_.setVisible(true))
       loadingIndicatorHolder.foreach(_.hide())
     }
 
@@ -180,8 +427,8 @@ class CurrentUserProfileFragment extends Fragment {
   }
 
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
-    val view = inflater.inflate(R.layout.fragment_user_profile, container, false)
-    updateProfile(userIDHolder.getOrElse(-1L))
+    val view = inflater.inflate(R.layout.fragment_current_user_profile, container, false)
+    updateProfile()
     view
   }
 }
